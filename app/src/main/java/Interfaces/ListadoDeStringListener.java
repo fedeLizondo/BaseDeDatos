6f23efa91c70f83122ad870323a4acb546313a20
fedeLizondo <federicoalizondo@gmail.com>
2017-02-13 23:18:40 -0300

    Antes de Cambios en ListView por RecycleViews

---
+++ app/src/main/java/Interfaces/ListadoDeStringListener.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+import java.util.ArrayList;
+
+/**
+ * Created by federicolizondo on 13/02/17.
+ */
+
+public interface ListadoDeStringListener {
+    public ArrayList<String> pulsado();
+}
